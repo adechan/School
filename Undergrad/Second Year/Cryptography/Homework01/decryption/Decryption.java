21993883e4a067cb4219c7179da2b81928b40491
adechan <ade.enter@yahoo.com>
2019-02-26 22:31:05 +0200

    added homework1 to Cryptography
    An implementation of a simple cipher. This is supposed to encrypt a plaintext using a key (key which is a permutation of the alphabet). And decrypt the cryptotext and find the key.

--- Undergrad/Second Year/Cryptography/Homework01/decryption/Decryption.java
+++ Undergrad/Second Year/Cryptography/Homework01/decryption/Decryption.java
@@ -1,5 +1,11 @@
 package decryption;
-
+/**
+ * This class is supposed to take a cryptotext and try to decrypt it until you can get a readable text.
+ * The decryption is made using symbol frequency, the most used symbol found in cryptotext is probably
+ * the most used symbol in english.
+ * @author Andreea Rindasu
+ *
+ */
 public class Decryption {
 
 }
--- Undergrad/Second Year/Cryptography/Homework01/main/Main.java
+++ Undergrad/Second Year/Cryptography/Homework01/main/Main.java
@@ -1,16 +1,19 @@
 package main;
 import java.io.*;
 import filtration.Filtration;
+import encryption.Encryption;
+import encryption.Key;
 
 public class Main {
+	
+	public static void rest()
+	{		
+	}
 
 	public static void main(String[] args)
 	{
-		
-		/*
-		 * reading from file 
-		 * line by line
-		 */
+
+		// reading from a file line by line
 		String getArg = args[0];
 		
 		try
@@ -18,27 +21,27 @@ public class Main {
 			FileReader reader = new FileReader(getArg);
 			BufferedReader bufferedReader = new BufferedReader(reader);
 			
-			String line;
-			
 			FileWriter writer = new FileWriter("writeFile.txt", true);
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
+			
+			Key key = new Key();
+			System.out.println(Utility.arrayToString(Key.alphabet));
+			System.out.println(key);
+			
+			String line;
 			
 			// while we still have lines to read
 			while ((line = bufferedReader.readLine()) != null)
 			{
 				// we have to remove everything BUT NOT A-Z
 				// and make a-z A-Z
-				// System.out.println(Filtration.GetFiltratedString(line));
-				
-				/*
-				 * bufferedWriter.write(line);
-				 *	bufferedWriter.write('\n');
-				*/
-				bufferedWriter.write(Filtration.GetFiltratedString(line));
+				String filtered = Filtration.getFiltratedString(line);
+				bufferedWriter.write(Encryption.encryptPlaintext(filtered, key));
 			}
 
 			bufferedWriter.close();
 			reader.close();
+			
 		}
 		
 		catch (IOException e)
