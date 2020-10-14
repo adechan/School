e0f755fba6c79b9a31e88d19466a1379f5d7e521
adechan <ade.enter@yahoo.com>
2020-10-14 18:40:03 +0300

    Update exercise12.py

--- Undergrad/Third Year/Python 2020/Lab 2/exercise12.py
+++ Undergrad/Third Year/Python 2020/Lab 2/exercise12.py
@@ -2,7 +2,6 @@
 # as parameter and will return a list of lists of words,
 # grouped by rhyme. Two words rhyme if both of them end
 # with the same 2 letters.
-from typing import Dict, Any
 
 
 def groupByRhyme(words):
