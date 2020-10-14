adc4d81934d7675f239daeda22b25562794196e8
adechan <ade.enter@yahoo.com>
2020-10-14 18:37:43 +0300

    Update exercise12.py

--- Undergrad/Third Year/Python 2020/Lab 2/exercise12.py
+++ Undergrad/Third Year/Python 2020/Lab 2/exercise12.py
@@ -2,12 +2,24 @@
 # as parameter and will return a list of lists of words,
 # grouped by rhyme. Two words rhyme if both of them end
 # with the same 2 letters.
+from typing import Dict, Any
+
 
 def groupByRhyme(words):
-    group = []
 
+    lastLetters = dict()
+
+    for word in words:
+        if word[len(word) - 2 : len(word) + 1] not in lastLetters.keys():
+            lastLetters[word[len(word) - 2 : len(word) + 1]] = [word]
+        else:
+            lastLetters[word[len(word) - 2 : len(word) + 1]].append(word)
+
+    group = []
+    for key in lastLetters:
+        group.append(lastLetters.get(key))
 
     return group
 
 
-print(groupByRhyme(['ana', 'banana', 'lana', 'carte', 'arme', 'parte']))
+print(groupByRhyme(['ana', 'banana', 'carte', 'arme', 'parte']))
